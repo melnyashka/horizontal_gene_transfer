71ed60b37738108eae67948a5a598c3cfd6dbf7b
Samuel <samnordmann@gmail.com>
2018-07-23 16:43:10 +0200

    basic modifications

--- hgt_stochastic.py
+++ hgt_stochastic.py
@@ -4,9 +4,17 @@ import numpy as np
 
 
 # Auxiliary part
+T_max= 10 #Maximal time
+dT=0.1 #Step of discretization for time
+nT=int(T_max/dT) #number of times
+T=[t*dT for t in range(nT)] #list of all times
+
 
 X_min = 0   # Minimal trait
 X_max = 4   # Maximum amount of traits
+dX=0.1 #Step of discretization for time
+nX=int((X_max-X_min)/dX) #number of traits
+X=[x*dX for x in range(nX)] #list of possible traits
 
 K = 1000    # Maximal capacity of the system
 C = 0.5     # Competition
@@ -15,6 +23,13 @@ p = 0.03    # Probability of mutation
 sigma = 0.1     # variance of the mutation kernel
 
 N_0 = 1000 # Initial number of populations 
+
+N = [[0] * nX for _ in range(nT)] #Intialization of the population. Matrix T*X
+#N[0]=N_0
+
+
+
+
 
 def b(x):   # Birth rate function
     return 4-x
