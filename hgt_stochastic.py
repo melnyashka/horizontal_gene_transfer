72b423f87663b050b6d2fcfbde4ba59dee45b184
melnyashka <ann.melnyashka@gmail.com>
2018-07-23 17:12:42 +0200

    Slight modification

--- hgt_stochastic.py
+++ hgt_stochastic.py
@@ -1,7 +1,7 @@
 # Import packages here
 
 import numpy as np
-
+import random as rnd
 
 # Auxiliary part
 T_max= 10 #Maximal time
@@ -9,39 +9,32 @@ dT=0.1 #Step of discretization for time
 nT=int(T_max/dT) #number of times
 T=[t*dT for t in range(nT)] #list of all times
 
-
 X_min = 0   # Minimal trait
 X_max = 4   # Maximum amount of traits
-dX=0.1 #Step of discretization for time
-nX=int((X_max-X_min)/dX) #number of traits
-X=[x*dX for x in range(nX)] #list of possible traits
+dX = 0.1      # Step of discretization for time
+nX = int((X_max-X_min)/dX) # number of traits
+X = [x*dX for x in range(nX)] # list of possible traits
 
 K = 1000    # Maximal capacity of the system
 C = 0.5     # Competition
 p = 0.03    # Probability of mutation
-beta=0
-mu=1
+beta = 0    # 
+mu = 1      # 
 
 sigma = 0.1     # variance of the mutation kernel
 
-N_0 = 1000 # Initial number of populations 
+N_0 = 1000*rnd.gauss(1, K) # Initial number of populations 
 
 N = [[0] * nX for _ in range(nT)] #Intialization of the population. Matrix T*X
-#N[0]=N_0
 
-
-
-
+def mut_kern(x):
+    return rnd.gauss(0, sigma^2)
 
 def b(x):   # Birth rate function
     return 4-x
 
-
 def d(x):   # Death rate function
     return 1
-
-def Ntot(N,t):#number of individual, at time t for population N
-    return sum(N[t])
 
 def death_prob(x, Ntot):
     # nx = number of individuals in a population with a trait x
@@ -50,13 +43,13 @@ def death_prob(x, Ntot):
 
 # Compute the probability of birth, death and horizontal transfer events
 
-def tau(x,y):#function tau
+def tau(x,y):   # function tau
     if (x<y): 
         return 1 
     else:
         return 0
     
-def horizontal_transfer(x, y, Ntot):#HT rate, i.e tau/(beta+mu*Ntot)
+def horizontal_transfer(x, y, Ntot):    # HT rate, i.e tau/(beta+mu*Ntot)
     return tau(x,y)/(beta+mu*Ntot)
 
 
