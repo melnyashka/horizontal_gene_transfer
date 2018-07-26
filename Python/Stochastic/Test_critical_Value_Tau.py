02b77b80c0332dfcd647d5e6d1877d683377a7cf
Samuel <samnordmann@gmail.com>
2018-07-26 17:26:50 +0200

    a

--- Python/Stochastic/Test_critical_Value_Tau.py
+++ Python/Stochastic/Test_critical_Value_Tau.py
@@ -5,6 +5,8 @@ Created on Thu Jul 26 16:14:55 2018
 
 @author: samuelnordmann
 """
+import gc
+
 
 # import stochastic_continuous # Check if it works on your machine! 
 parameters = dict(T_max = 1000, # maximal time 
@@ -28,36 +30,46 @@ parameters = dict(T_max = 1000, # maximal time
 # idea: create a grid of parameters we want to check, and then run the experiments inside the loop! 
 
 X0 = np.random.normal(parameters['x_mean0'], parameters['sigma0'], parameters['N0']) # Initial population
+X0.sort()
 #X = [None]*int(parameters['T_max']/parameters['dT'])  # history of all populations up to time T_max
-X = np.sort(X0)
+
 
 Abs=[]   
 Ord=[]
 
 
-tau_M=3
+tau_M=5
 tau_m=0.001
-tau_step=0.05
+tau_step=0.1
 
 param='b_r'
 param_M=10
 param_m=1
 param_step=1
 
+for param_value in np.arange(param_m,param_M,param_step):
+    parameters[param]=param_value
+    print(param_value)
+    for tau in np.flip(np.arange(tau_m,tau_M,tau_step),0):
+        parameters['tau']=tau
+        X = X0
+        for i in range(int(parameters['T_max']/parameters['dT']-1)):
+            X=Next_Generation(X, parameters)
+            if X.size<10:#If the population dies, we go out of the loop
+                break
+        if X.size<10:
+            continue
+        Ord.append(parameters['tau'])
+        Abs.append(parameters[param])
+        print(Ord)
+        print(Abs)
+        break
 
-for tau in np.flip(np.arange(tau_m,tau_M,tau_step),0):
-    parameters['tau']=tau
-    for i in range(int(parameters['T_max']/parameters['dT']-1)):
-        X=Next_Generation(X, parameters)
-        if X.size<10:#If the population dies, we go out of the loop
-            break
-    if X.size<10:
-        continue
-    Ord.append(parameters['tau'])
-    Abs.append(parameters[param])
-    break
 
 
+
+
+gc.collect()
 
 
 
