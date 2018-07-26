0bc22741918d090c070976e74148972fb4ae4f51
Samuel <samnordmann@gmail.com>
2018-07-26 10:32:32 +0200

    a

--- Python/Stochastic/Test.py
+++ Python/Stochastic/Test.py
@@ -13,11 +13,22 @@ Ord=[]
 for i in range(int(T_max/dT-1)):
     X[i+1]=Next_Generation(X[i])
     for x in X[i]:
-        Abs.extend([i])
+        Abs.extend([i*dT])
         Ord.extend([x])
 
 
 plt.scatter(Abs,Ord,s=0.5)
+plt.xlabel('time')
+plt.ylabel('trait');
+
+
+titre='T_max = '+str(T_max)
++'\ndT = '+str(dT) 
++'\nK = '+str(K)
++'\nN0 = '+str(N0)'
+
+plt.title(titre)
+
 plt.show()
 
 
