285deb8761a9257ce8da2f95150e5958013a5127
melnyashka <ann.melnyashka@gmail.com>
2018-07-31 15:57:27 +0200

    modified PDE

--- Python/PDE/PDE.py
+++ Python/PDE/PDE.py
@@ -1,35 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import gc 
-
-def i_to_x(f, parameters): # return the vector of traits by given vector of densities
+def compute_things(parameters):
     L, dX = parameters['L'], parameters['dX']
-    return  -L+i*dX
-
-I_to_x=np.vectorize(lambda i: i_to_x(i, parameters),otypes=[float])
-
-
-
-
-def f_to_x(f, parameters): # return the vector of traits by given vector of densities
-    L, dX = parameters['L'], parameters['dX']
-    return list(map(lambda i: -L+i*dX, range(int(2*L/dX))))
-
-
+    X_min, X_max= -L, L
+    nX = int((X_max-X_min)/dX)
+    # Create a new dictionary with: computed vector of traits, computed birth/death rate
+    X = np.vectorize(lambda i: -parameters['L']+i*parameters['dX'],otypes=[float])(range(nX)) # reconstruct traits by index
+    death_kernel = np.vectorize(lambda x: parameters['d_r']*np.power(np.absolute(x),parameters['d_e']))(X) 
+    # ht_kernel = np.vectorize(lambda x: np.dot(parameters['tau'],np.heaviside(x-X, 1)))(X)
+    
+    init_density = np.exp(-np.power(np.absolute(X),2)/parameters['sigma0']*parameters['eps'])
+    new_dict = {'X' : X,
+                'death_kernel': death_kernel, 
+                'init_density': init_density}
+    return new_dict    
 
 def tau_ht(x,y, n_tot, parameters): # horizontal transfer function
     tau, beta, mu = parameters['tau'], parameters['beta'], parameters['sigma']
     return tau*np.divide(np.heaviside(y-x, 1), (beta+n_tot*mu))
 
-def Next_Generation_PDE(f,parameters):
-    dT, b_r, d_r, d_e, K, C  = parameters['dT'], parameters['b_r'], parameters['d_r'], parameters['d_e'], parameters['K'], parameters['C']
+def Next_Generation_PDE(f,parameters, pre_values):
+    dT, b_r, dX, sigma = parameters['dT'], parameters['b_r'], parameters['dX'], parameters['sigma']
+    x = pre_values['X']
     n_tot = np.sum(f)
-    x = np.array(f_to_x(f, parameters))
-    death = d_r*np.power(np.absolute(x),d_e) + n_tot*C/K
-    dX, eps = parameters['dX'], parameters['eps']
-    birth_part = np.array(list(map(lambda y: b_r*np.sum(f*np.exp(-np.abs(x-y)/eps))*dX, x))) # birth part of the integro-differential equation
-    transfer_part = (f*np.array(list(map(lambda y: np.sum(np.divide(tau_ht(x,y,n_tot,parameters)*f, n_tot)), x))))*dX 
+    death = pre_values['death_kernel'] + n_tot*parameters['C']/parameters['K']
+    birth_part = b_r/sigma*dX*np.vectorize(lambda y: np.sum(np.dot(f,np.exp(-np.abs(y-x)))), otypes=[float])(x)
+    transfer_part = np.vectorize(lambda y: np.sum(np.divide(tau_ht(x,y,n_tot,parameters)*f, n_tot)),otypes=[float])(x)
     new_f = f - dT*(death+n_tot)*f + birth_part + transfer_part
     gc.collect()
     return new_f
-
