dc93cf6aecd7aebe09f13be17d0b44e7b4d1dab8
melnyashka <ann.melnyashka@gmail.com>
2018-07-31 11:42:37 +0200

    Added test

--- Python/PDE/PDE.py
+++ Python/PDE/PDE.py
@@ -2,32 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import gc 
 
-parameters = dict(T_max = 1000, # maximal time 
-                  dT = 0.1, # Discretization time 
-                  K = 1000, # Maximal capacity of the system
-                  N0 = 1000,    # Initial number of population
-                  sigma0 = 0.1,  #Initial standard variation of the population
-                  x_mean0 = 0.,
-                  C = 0.5,    # competition
-#                 p = 0.03,      # Probability of mutation
-                  b_r = 1,     # birth rate
-                  d_r = 1,      # death rate
-                  d_e = 2,   #exponetial power
-                  beta = 0, 
-                  mu = 1,
-                  sigma = 0.01,
-                  tau = 0.17,  # transfer rate
-                  L = 10, #length of the numerical interval of traits (for PDE!)
-                  dX = 0.001, #discretization of the space of traits
-                  eps = 1
-                  )
-
-L, dX = parameters['L'], parameters['dX']
-X_min, X_max= -L, L
-nX = int((X_max-X_min)/dX) # number of traits
-X = np.arange(0,dX*nX,dX) # list of possible traits
-F0 = np.exp(-np.power(np.absolute(X),2)/parameters['sigma0']*parameters['eps']) # initial density 
-
 def x_to_i(x, parameters): # return the index by the trait
     return int(np.divide((x + L), dX))
 
---
+++ Python/PDE/PDE_test.py
@@ -0,0 +1,30 @@
+parameters = dict(T_max = 100, # maximal time 
+                  dT = 0.1, # Discretization time 
+                  K = 1000, # Maximal capacity of the system
+                  N0 = 1000,    # Initial number of population
+                  sigma0 = 0.1,  #Initial standard variation of the population
+                  x_mean0 = 0.,
+                  C = 0.5,    # competition
+#                 p = 0.03,      # Probability of mutation
+                  b_r = 1,     # birth rate
+                  d_r = 1,      # death rate
+                  d_e = 2,   #exponetial power
+                  beta = 0, 
+                  mu = 1,
+                  sigma = 0.01,
+                  tau = 0.17,  # transfer rate
+                  L = 3, #length of the numerical interval of traits (for PDE!)
+                  dX = 0.001, #discretization of the space of traits
+                  eps = 1
+                  )
+
+L, dX, T_max, dT = parameters['L'], parameters['dX'], parameters['T_max'], parameters['dT']
+X_min, X_max= -L, L
+nX = int((X_max-X_min)/dX) # number of traits
+X = np.arange(0,dX*nX,dX) # list of possible traits
+F0 = np.exp(-np.power(np.absolute(X),2)/parameters['sigma0']*parameters['eps']) # initial density 
+XT = np.empty([int(T_max*dT), nX])
+XT[0] = F0
+
+for i in range(int(T_max*dT)-1):
+    XT[i + 1] = Next_Generation_PDE(XT[i])
